ba002139e57aaf40cff4bd0de5452dcb13330f7d
Jhair14 <129466823+Jhair14@users.noreply.github.com>
2025-08-20 00:01:16 -0400

    v2-front

--- app/config/apollo.js
+++ app/config/apollo.js
@@ -3,8 +3,7 @@ import {ApolloClient, createHttpLink, InMemoryCache} from "@apollo/client";
 const client = new ApolloClient({
     cache: new InMemoryCache(),
     link: createHttpLink({
-        uri: "http://34.28.246.100:4000/",
-        //uri: "http://localhost:4000/",
+        uri: "https://alaschiquitanasapi-production.up.railway.app/",
         fetch
     })
 })
